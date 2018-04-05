ff8791b2d2a94840a47eaf7443d5872ffdabc1dc
sandrogaoyun <38107257+sandrogaoyun@users.noreply.github.com>
2018-04-05 23:17:38 +0800

    2

--- 特殊回文数.py
+++ 特殊回文数.py
@@ -5,6 +5,7 @@
 
 n = int(input("请输入(5<=n<=54)：")) #先不考虑各种字符检测
 
+#最土的做法
 #计算满足条件的5位数
 for i in range (1, 10): #第一、五位数字
     for j in range (0, 10): #第二、四位数字
@@ -20,3 +21,16 @@ for i in range (1, 10): #第一、六位数字
             if i + j + k + k + j + i == n:
                 tmp = i * 100001 + j * 10010 + k * 1100
                 print(tmp)
+
+#好的做法
+def reverse(n):
+    # 返回一个整数的逆序
+    return int(str(n)[::-1])
+
+def accumulate(n):
+    # 返回一个整数的各位数之和
+    return sum([int(x) for x in str(n)])
+
+for i in range(10000,1000000):
+    if i == reverse(i) and accumulate(i) ==n:
+        print(i)
